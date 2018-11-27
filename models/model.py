4b2362b097d25d95307e83efc609747a18ccd0dd
WS Team <WSTeam@epsi.fr>
2018-11-27 16:16:28 +0000

    fix database path
    Signed-off-by: WS Team <WSTeam@epsi.fr>

--- models/model.py
+++ models/model.py
@@ -4,6 +4,6 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/ubuntu/db/bm2050.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/ubuntu/bm2050/db/bm2050.db'
 db = SQLAlchemy(app)
 
