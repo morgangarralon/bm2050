df25a0717e0d55dd0978a835ac2f3c83084e4921
WS Team <WSTeam@epsi.fr>
2018-11-27 17:28:14 +0000

    use conf file to configure db path
    Signed-off-by: WS Team <WSTeam@epsi.fr>

--- models/model.py
+++ models/model.py
@@ -2,8 +2,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+try:
+    from .conf import DB_PATH
+except ModuleNotFoundError as e:
+    print("configure 'models/conf.py' from 'models/conf.py.sample'")
+    exit(1)
+
+
 app = Flask(__name__)
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/ubuntu/bm2050/db/bm2050.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////{}'.format(DB_PATH)
 db = SQLAlchemy(app)
 
--- populate_db.py
+++ populate_db.py
@@ -1,4 +1,4 @@
-
+#!/usr/bin/env python3
 
 from models.model import db
 from models.account import Account
