d7e963e31dad928685d9b032bc82cd8412384ad3
ErikSteNL <erikstevens79137@gmail.com>
2018-11-28 11:40:49 +0100

    created simple populate db

--- populate_db.py
+++ populate_db.py
@@ -2,13 +2,27 @@
 
 from models.model import db
 from models.account import Account
+from models.answerStatus import AnswerStatus
 
-for email in ['toto@epsi.fr', 'titi@epsi.fr']:
-    a = Account(email)
-    a.IsAdmin = False
-    a.FirstName = email
-    db.session.add(a)
-    db.session.commit()
+import datetime
 
-print(Account.query.all())
 
+acc = Account()
+acc.IsAdmin = False
+acc.FirstName = 'Erik'
+acc.LastName = 'Stevens'
+acc.EmailAddress = 1
+acc.RoleId = 1
+acc.CreationDate = datetime.datetime.now()
+acc.LastLogin = datetime.datetime.now()
+acc.Username = 'Erik'
+acc.Password = 'Stevens'
+
+db.session.add(acc)
+db.session.commit()
+
+ans = AnswerStatus()
+ans.Name = 'Done'
+
+db.session.add(ans)
+db.session.commit()
