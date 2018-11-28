6d181cab421f2e01e6185ae06e3d6c42ce06a6df
momomo <morgan.garralon@gmail.com>
2018-11-28 16:58:53 +0100

    404 page + minor fixes

--- app.py
+++ app.py
@@ -27,18 +27,22 @@ app.config.from_mapping(
 
 model.db.init_app(app)
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 @app.route('/')
 def hello_world():
     topics = TopicController.findAllTopic()
     return render_template('index.html', static_url_path = static_url_path, topic_list='lol', topics = topics)
 
-@app.route('/addTopic', methods = ['GET', 'POST'])
+@app.route('/add_topic', methods = ['GET', 'POST'])
 def addTopic():
     if request.method == 'GET':
-        return render_template('AddTopic.html', title='template test')
+        return render_template('add_topic.html', title='template test')
     elif request.method == 'POST':
         TopicController.createTopic(request.form.get('question'), 1, 1, True)
-        return render_template('AddTopic.html', title='template test')
+        return render_template('add_topic.html', title='template test')
 
 # @app.route('/models')
 # def models():
@@ -46,7 +50,7 @@ def addTopic():
 #     a.Password = 'helloworld'
 #     return '<p> {} </p>'.format(a.EmailAddress)
 
-@app.route('/updateTopicScore/<id>/<val>', methods = ['GET', 'POST'])
+@app.route('/update_topic_score/<id>/<val>', methods = ['GET', 'POST'])
 def updateTopicScore(id, val):
     TopicController.updateTopicScore(id, val)
     return displayTopic(id)
@@ -56,7 +60,7 @@ def index():
     topics = TopicController.findAllTopic()
     return render_template('index.html', static_url_path = static_url_path, topic_list='lol', topics = topics)
 
-@app.route('/displayTopic/<topic_id>')
+@app.route('/display_topic/<topic_id>')
 def displayTopic(topic_id = None):
     template = None;
 
