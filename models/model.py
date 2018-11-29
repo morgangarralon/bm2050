d51f35792a702dc1d143725995827779ed9aab6e
WS Team <WSTeam@epsi.fr>
2018-11-29 09:35:31 +0000

    clean code
    Signed-off-by: WS Team <WSTeam@epsi.fr>

--- models/model.py
+++ models/model.py
@@ -1,6 +1,5 @@
 import os
 import sys
-# import sqlite3
 from flask_sqlalchemy import SQLAlchemy
 
 try:
@@ -13,11 +12,8 @@ db_path = os.path.join(os.path.dirname(__file__) + '/../' + DB_DIR, DB_NAME)
 track_modification = False
 database_uri = 'sqlite:///{}'.format(db_path)
 
-# print(db_path)
-
 db = SQLAlchemy()
 
 def init_app(app):
-	# app._database = sqlite3.connect(db_path)
-	db.init_app(app)
-	db.app = app
+    db.init_app(app)
+    db.app = app
